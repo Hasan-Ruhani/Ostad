818c4720ec284bcf62d2fc3e6253538d26116ebf
H A S A N <hasan25042019@gmail.com>
2023-02-20 21:50:30 +0600

    Create problem-4.php
    df

---
+++ Assignment_of_Module-4/problem-4.php
@@ -0,0 +1,23 @@
+<?php
+   function say($string){
+      $needle = 'H';
+      $isWPresent = strpos($string, $needle);
+
+      if($needle == ' '){     //check whitespace
+      $needle = "' '";
+      }
+
+      if($isWPresent !== false){     //check letters
+      echo "{$needle} is present";
+      }
+      else{
+      echo "{$needle} is not present";
+      }
+   }
+   say("Hi I'm Hasan");
+   
+
+
+      
+      
+    
