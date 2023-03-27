26e097d5bc0ec8990b05904cc86a470b760bd488
H A S A N <hasan25042019@gmail.com>
2023-03-27 15:46:34 +0600

    Update login.php
    ddf

--- Assignment_of_Module-8/login.php
+++ Assignment_of_Module-8/login.php
@@ -10,6 +10,14 @@
         <title>login</title>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">    
     </head>
+	<style>
+		p {
+			color: red;
+		}
+		h12 {
+			color: red;
+		}
+	</style>
 	<body>
 		<section class="h-100 gradient-form" style="background-color: #eee;">
 			<div class="container py-5 h-100">
@@ -24,19 +32,20 @@
 										<img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
 											style="width: 185px;" alt="logo">
 										<h4 class="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+										<p><?php echo $inv_msg?></p>
 										</div>
 
-										<form method="POST" action="login.php">
-											<p>Please login to your account</p>
+										<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+											<p1>Please login to your account</p1>
 
 											<div class="form-outline mb-4">
-												<?php echo $inv_email, $inv_email_blank; ?>
+												<h12><?php echo $inv_email, $inv_email_blank; ?></h12>
 												<input type="text" id="eamil" name="email" class="form-control" placeholder="Email address">
 												<label class="form-label" for="form2Example11">Email</label>
 											</div>
 
 											<div class="form-outline mb-4">
-												<?php echo $inv_pass; ?>
+												<h12><?php echo $inv_pass; ?></h12>
 												<input type="password" id="pass" name="pass" class="form-control" placeholder="Password">
 												<label class="form-label" for="form2Example22">Password</label>
 											</div>
