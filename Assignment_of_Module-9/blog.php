d2523f7c0e0fdf0a0b01b2c30a70f6b9997c20a9
H A S A N <hasan25042019@gmail.com>
2023-04-03 17:23:31 +0600

    Create blog.php
    fg

---
+++ Assignment_of_Module-9/blog.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Blog - Latest Posts</title>
+	<link rel="stylesheet" href="assets/blog.css">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">    
+
+</head>
+<body>
+<?php include_once("header.php"); ?>
+
+	<main>
+		<section id="blog-posts">
+			<div class="container">
+				<h1>BLOG</h1>
+
+				<div class="blog-post">
+				<a href="data/post1.php"><h2>BLOG POST-01</h2></a>
+					<img src="https://cdn.pixabay.com/photo/2023/01/23/17/28/tree-7739243_960_720.jpg" alt="Blog Post Image">
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat quisquam,
+						quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi unde
+						natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+						quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi 
+						unde natusLorem ipsum dolor sit adipisicing elit. Optio laborum provident<a href="data/post1.php">Read more...</a> 
+					</p>				
+					</div>
+
+				<div class="blog-post">
+				<a href="data/post2.php"><h2>BLOG POST-02</h2></a>
+					<img src="https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_960_720.jpg" alt="Blog Post Image">
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat quisquam,
+						quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi unde
+						natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+						quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi 
+						unde natusLorem ipsum dolor sit adipisicing elit. Optio laborum provident<a href="data/post2.php">Read more...</a> 
+					</p>				
+				</div>
+
+				<div class="blog-post">
+				<a href="data/post3.php"><h2>BLOG POST-03</h2></a>
+					<img src="https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg" alt="Blog Post Image">
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat quisquam,
+						quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi unde
+						natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+						quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi 
+						unde natusLorem ipsum dolor sit adipisicing elit. Optio laborum provident<a href="data/post3.php">Read more...</a> 
+					</p>			
+					</div>
+
+				<div class="blog-post">
+				<a href="data/post4.php"><h2>BLOG POST-04</h2></a>
+					<img src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_960_720.jpg" alt="Blog Post Image">
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat quisquam,
+						quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi unde
+						natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+						quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi 
+						unde natusLorem ipsum dolor sit adipisicing elit. Optio laborum provident<a href="data/post4.php">Read more...</a> 
+					</p>				
+				</div>
+
+				<div class="blog-post">
+				<a href="data/post5.php"><h2>BLOG POST-05</h2></a>
+					<img src="https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509_960_720.jpg" alt="Blog Post Image">
+					<p>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat quisquam,
+						quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi unde
+						natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+						quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi 
+						unde natusLorem ipsum dolor sit adipisicing elit. Optio laborum provident<a href="data/post5.php">Read more...</a> 
+					</p>				
+				</div>
+			</div>
+		</section>
+	</main>
+	<?php include_once("footer.php"); ?>
+</body>
+</html>
