91f33a7d8537567b11b3c181e0e3147f981dca69
H A S A N <hasan25042019@gmail.com>
2023-04-03 17:22:41 +0600

    Update index.php
    ddf

---
+++ Assignment_of_Module-9/index.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HOME</title>
+        <link rel="stylesheet" href="assets/styles.css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">    
+    </head>
+    
+    <body>
+    <style>
+      body{
+        background-color: #615d8329;
+      }
+    </style>
+        <?php include_once("header.php"); ?>
+
+        <section class="featured-post">
+  <h2>Welocme to new Blog site</h2>
+ 
+
+  <div class="media">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1"><a href="data/post1.php">BLOG POST-01</a></h5>
+        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+            quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta 
+            nisi unde natus
+        </p>
+      </div>
+      <a href="data/post1.php"><img class="" src="https://cdn.pixabay.com/photo/2023/01/23/17/28/tree-7739243_960_720.jpg" alt="Featured Post Image"></a>
+  </div>
+
+  <div class="media">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1"><a href="data/post2.php">BLOG POST-02</a></h5>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+        quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta 
+        nisi unde natus
+      </p>
+        </div>
+        <a href="data/post2.php"><img class="rounded" src="https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667_960_720.jpg" alt="Featured Post Image"></a>
+      </div>
+
+  <div class="media">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1"><a href="data/post3.php">BLOG POST-03</a></h5>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+        quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta 
+        nisi unde natus
+      </p>
+        </div>
+        <a href="data/post3.php"><img class="ml-3" src="https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg" alt="Featured Post Image"></a>
+      </div>
+
+  <div class="media">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1"><a href="data/post4.php">BLOG POST-04</a></h5>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+        quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta 
+        nisi unde natus
+      </p>
+        </div>
+        <a href="data/post4.php"><img class="ml-3" src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_960_720.jpg" alt="Featured Post Image"></a>
+      </div>
+
+  <div class="media">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1"><a href="data/post5.php">BLOG POST-05</a></h5>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+        quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta 
+        nisi unde natus
+      </p>
+        </div>
+        <a href="data/post5.php"><img class="ml-3" src="https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509_960_720.jpg" alt="Featured Post Image"></a>
+      </div>
+
+</section>
+
+<?php include_once("footer.php"); ?>
+    </body>
+</html>
