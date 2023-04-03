5bbe5981c86dc5f0f20554a4f8a77cb501a8787c
H A S A N <hasan25042019@gmail.com>
2023-04-03 17:23:40 +0600

    Create post4.php
    fgf

---
+++ Assignment_of_Module-9/data/post4.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">    
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="../index.php">HOME</a>
+    <a class="navbar-brand" href="../blog.php">BLOG</a>
+    <a class="navbar-brand" href="../contact.php">CONTACT</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    <img src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_960_720.jpg" alt="Featured Post Image">
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat quisquam,
+        quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi unde
+        natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum provident placeat 
+        quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque dicta nisi 
+        unde natusLorem ipsum dolor sit adipisicing elit. Optio laborum provident 
+        placeat quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a itaque 
+        dicta nisi unde natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio laborum 
+        provident placeat quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt hic nemo a 
+        itaque dicta nisi unde natusLorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
+        laborum provident placeat quisquam, quas ratione ipsum libero non sint incidunt quasi deserunt
+        hic nemo a itaque dicta nisi unde natus
+      </p>    
+      
+      
+      <?php include_once("../footer.php") ?>
+</body>
+</html>
