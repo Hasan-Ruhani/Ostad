e182dbd58c2504c150c26d50ddc54d202d4d29ca
H A S A N <hasan25042019@gmail.com>
2023-04-10 23:58:15 +0600

    Create index.php
    fggf

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>NAV</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    
+    <style>
+      .navbar-brand {
+        font-size: 1.5rem;
+        font-weight: bold;
+      }
+      
+      .nav-item.active {
+        background-color: #0dcaf0;
+        border-radius: 5px;
+      }
+    </style>
+  </head>
+  
+  <body>
+    <!-- Navigation bar -->
+    <nav class="navbar navbar-expand-lg navbar navbar-light" style="background-color: #e3f2fd;">
+      <a class="navbar-brand" href="#">Logo</a>
+      
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="--">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="--">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="--">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
