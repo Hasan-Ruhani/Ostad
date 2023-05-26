394a9d89e5b889a1c949ec70076a75abe6ccfa72
H A S A N <hasan25042019@gmail.com>
2023-05-26 18:24:22 +0600

    initial commit

--- Assignment_of_Module-15/Laravel-Concepts/routes/api.php
+++ Assignment_of_Module-15/Laravel-Concepts/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\laravelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::controller(laravelController::class) -> group(function(){
-    Route::get('/page/{text?}', 'getQuery');                     //Question 03
 });
